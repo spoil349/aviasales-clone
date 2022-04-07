256c99396ffcb0a6e850490d0994aaef862cfff0
spoil349 <spoil349@mail.ru>
2022-04-08 01:22:18 +0400

    feat: add LoadMore components styles

--- src/components/LoadMore.vue
+++ src/components/LoadMore.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="load-more">
-        <button class="load-more__button" type="button">Показать еще 5 билетов!</button>
-    </div>
+    <button class="load-more" type="button">Показать еще 5 билетов!</button>
 </template>
 
 <script>
@@ -13,9 +11,28 @@ export default {
 </script>
 
 <style lang="scss">
-    .load-more {
-        &__button {
-            text-transform: uppercase;
-        }
+@import '../styles/variables.scss';
+
+.load-more {
+    width: 100%;
+    text-transform: uppercase;
+    border-radius: 5px;
+    background-color: $blue;
+    color: $white;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    border: none;
+    cursor: pointer;
+    transition: all ease .1s;
+
+    &:hover {
+        filter: drop-shadow(2px 1px 3px $blue);
     }
+
+    &:active {
+        position: relative;
+        transform: translateY(3%);
+        filter: drop-shadow(2px 1px 3px $blue) grayscale(.3);
+    }
+}
 </style>
